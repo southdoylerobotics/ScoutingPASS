ff9b12027c3f77f4a69ab10a25c6d2f6edde548f
South-Doyle Robotics <34195060+southdoylerobotics@users.noreply.github.com>
2025-03-01 14:21:47 -0500

    Update TBAInterface.js
    Updated authKey

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "On8SkS3Kvz4yrbkhPPnSPkPTtUEyrAUr7vP5AMenPqUfVHbMjYKGmooBkjphoxAS";
 /**
  * Get list of teams in event
  *
