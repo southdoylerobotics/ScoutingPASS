af3b82a7d2ef7619c120ddcc28f2e4b4fb5483b5
South-Doyle Robotics <34195060+southdoylerobotics@users.noreply.github.com>
2025-03-05 13:13:48 -0500

    Update reefscape_config.js
    Changed event to arkansas

--- 2025/reefscape_config.js
+++ 2025/reefscape_config.js
@@ -17,7 +17,7 @@ var config_data = `
             "name": "Event",
             "code": "e",
             "type": "event",
-            "defaultValue": "2025nhsal",
+            "defaultValue": "2025arli",
             "required": "true"
         },
         {
