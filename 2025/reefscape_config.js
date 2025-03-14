ac69bc58b1f122062de98dbb7a4bc57922a81665
South-Doyle Robotics <34195060+southdoylerobotics@users.noreply.github.com>
2025-03-14 09:23:24 -0400

    Update reefscape_config.js
    Alabama

--- 2025/reefscape_config.js
+++ 2025/reefscape_config.js
@@ -17,7 +17,7 @@ var config_data = `
             "name": "Event",
             "code": "e",
             "type": "event",
-            "defaultValue": "2025arli",
+            "defaultValue": "2025alhu",
             "required": "true"
         },
         {
