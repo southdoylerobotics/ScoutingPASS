153f715da746cbcdbf6ef67335a4602403920ab5
South-Doyle Robotics <34195060+southdoylerobotics@users.noreply.github.com>
2026-02-03 20:55:37 -0500

    Update rebuilt_pit_scouting.js
    updated pit scouting - max fuel capacity & got rid of a few things

--- 2026/rebuilt_pit_scouting.js
+++ 2026/rebuilt_pit_scouting.js
@@ -9,6 +9,11 @@ var config_data = `
       "code": "t",
       "type": "number"
     },
+    { "name": "Length",
+      "code": "len",
+      "type": "number",
+      "defaultValue": "0"
+    },
     { "name": "Width",
       "code": "wid",
       "type": "number",
@@ -18,37 +23,6 @@ var config_data = `
       "code": "wei",
       "type": "number",
       "defaultValue": "0"
-    },
-    { "name": "Drivetrain",
-      "code": "drv",
-      "type": "radio",
-      "choices": {
-        "s": "Swerve<br>",
-        "w": "West Coast/Tank<br>",
-        "b": "Butterfly/Grashopper<br>",
-        "m": "Mechanum<br>",
-        "o": "Other"
-      },
-      "defaultValue": "o"
-    },
-    { "name": "Other Drivetrain",
-      "code": "odt",
-      "type": "text",
-      "size": 20,
-      "maxSize": 50
-    },
-    { "name": "Swerve Ratio",
-      "code": "sr",
-      "type": "radio",
-      "choices": {
-        "1": "L1 (8.14:1)<br>",
-        "2": "L2 (6.75:1)<br>",
-        "3": "L3 (6.12:1)<br>",
-        "4": "L4 (5.14:1)<br>",
-        "o": "Other ratio (put in comments)<br>",
-        "x": "Not Swerve"
-      },
-      "defaultValue":"x"
     },
     { "name": "Drivetrain Motor",
       "code": "mot",
@@ -65,29 +39,9 @@ var config_data = `
       "code": "nob",
       "type": "number"
     },
-    { "name": "Floor pickup Fuel",
-      "code": "fpu",
-      "type": "bool"
-    },
-    { "name": "Depot pickup Fuel",
-      "code": "dpu",
-      "type": "bool"
-    },
-    { "name": "Outpost pickup Fuel",
-      "code": "opu",
-      "type": "bool"
-    },
-    { "name": "Autos",
-      "code": "aut",
-      "type": "text",
-      "size": 20,
-      "maxSize": 250
-    },
-    { "name": "Scouting Method /<br>Program (ScoutingPASS?)",
-      "code": "sct",
-      "type": "text",
-      "size": 20,
-      "maxSize": 250
+    { "name": "Max Fuel Capacity",
+      "code": "mfc",
+      "type": "number"
     },
     { "name": "Comments",
       "code": "co",
