aceec8ccd08c401945d897a475112840ea0f5b36
South-Doyle Robotics <34195060+southdoylerobotics@users.noreply.github.com>
2026-03-02 17:12:06 -0500

    Update rebuilt_config.js
    Fixing the code

--- 2026/rebuilt_config.js
+++ 2026/rebuilt_config.js
@@ -1,226 +1,47 @@
-var config_data = `
-{
+var config_data = `{
   "dataFormat": "tsv",
   "title": "Scouting PASS 2026",
   "page_title": "REBUILT",
   "checkboxAs": "10",
-  
- "prematch": [
-    { "name": "Scouter Initials",
-      "code": "s",
-      "type": "scouter",
-      "size": 5,
-      "maxSize": 5,
-      "required": "true"
-    },
-    { "name": "Your Team #",
-      "code": "yt",
-      "type": "number", 
-      "min": 1,
-      "max": 9999
-    },
-    { "name": "Event",
-      "code": "e",
-      "type": "event",
-      "defaultValue": "2026ilch",
-      "required": "true"
-    },
-    { "name": "Match Level",
-      "code": "l",
-      "type": "level",
-      "choices": {
-        "qm": "Quals<br>",
-        "sf": "Semifinals<br>",
-        "f": "Finals"
-      },
-      "defaultValue": "qm",
-      "required": "true"
-    },
-    { "name": "Match #",
-      "code": "m",
-      "type": "match",
-      "min": 1,
-      "max": 150,
-      "required": "true"
-    },
-    { "name": "Robot",
-      "code": "r",
-      "type": "robot",
-      "choices": {
-        "r1": "Red-1",
-        "b1": "Blue-1<br>",
-        "r2": "Red-2",
-        "b2": "Blue-2<br>",
-        "r3": "Red-3",
-        "b3": "Blue-3"
-      },
-      "required": "true"
-    },
-    { "name": "Team #",
-      "code": "t",
-      "type": "team",
-      "min": 1,
-      "max": 99999
-    },
-    { "name": "Auto Start Location",
-      "code": "as",
-      "type": "clickable_image",
-      "filename": "2026/half_field.png",
-      "clickRestriction": "one",
-      "dimensions": "7 10",
-      "allowableResponses": "4 11 18 25 32 39 46 53 60 67",
-      "shape": "circle 5 black red true"
-    }
+  "prematch": [
+    { "name": "Scouter Initials", "code": "s", "type": "scouter", "size": 5, "maxSize": 5, "required": "true" },
+    { "name": "Your Team #", "code": "yt", "type": "number", "min": 1, "max": 9999 },
+    { "name": "Event", "code": "e", "type": "event", "defaultValue": "2026ilch", "required": "true" },
+    { "name": "Match Level", "code": "l", "type": "level", "choices": { "qm": "Quals<br>", "sf": "Semifinals<br>", "f": "Finals" }, "defaultValue": "qm", "required": "true" },
+    { "name": "Match #", "code": "m", "type": "match", "min": 1, "max": 150, "required": "true" },
+    { "name": "Robot", "code": "r", "type": "robot", "choices": { "r1": "Red-1", "b1": "Blue-1<br>", "r2": "Red-2", "b2": "Blue-2<br>", "r3": "Red-3", "b3": "Blue-3" }, "required": "true" },
+    { "name": "Team #", "code": "t", "type": "team", "min": 1, "max": 99999 },
+    { "name": "Auto Start Location", "code": "as", "type": "clickable_image", "filename": "2026/half_field.png", "clickRestriction": "one", "dimensions": "7 10", "allowableResponses": "4 11 18 25 32 39 46 53 60 67", "shape": "circle 5 black red true" }
   ],
   "auton": [
-    { "name": "Fuel Scored",
-      "code": "afs",
-      "type": "counter"
-    },
-
-    { "name": "Climb (L1)",
-      "code": "ac",
-      "type": "radio",
-      "choices": {
-        "c": "Climbed<br>",
-        "a": "Attempted<br>",
-        "x": "Not Attempted"
-      },
-      "defaultValue": "x"
-    },
-    { "name": "Won Auto?",
-      "code": "wau",
-      "type": "bool"
-    }
+    { "name": "Fuel Scored", "code": "afs", "type": "counter" },
+    { "name": "Climb (L1)", "code": "ac", "type": "radio", "choices": { "c": "Climbed<br>", "a": "Attempted<br>", "x": "Not Attempted" }, "defaultValue": "x" },
+    { "name": "Won Auto?", "code": "wau", "type": "bool" }
   ],
   "teleop": [
-
-    { "name": "Fuel Scored: Transition Shift",
-      "code": "tsf",
-      "type": "counter"
-    },
-    { "name": "Fuel Scored: Shift 1",
-      "code": "sof",
-      "type": "counter"
-    },
-    
-    { "name": "Fuel Scored: Shift 2",
-      "code": "stf",
-      "type": "counter"
-    },
-    { "name": "Fuel Scored: End Game",
-      "code": "egf",
-      "type": "counter"
-    },     
-    { "name": "Pass from Neutral Zone",
-      "code": "pnz",
-      "type": "bool"
-    },
-    { "name": "Pass from Opp<br>Alliance Zone",
-      "code": "poa",
-      "type": "bool"
-    }
+    { "name": "Fuel Scored: Transition Shift", "code": "tsf", "type": "counter" },
+    { "name": "Fuel Scored: Shift 1", "code": "sof", "type": "counter" },
+    { "name": "Fuel Scored: Shift 2", "code": "stf", "type": "counter" },
+    { "name": "Fuel Scored: End Game", "code": "egf", "type": "counter" },
+    { "name": "Pass from Neutral Zone", "code": "pnz", "type": "bool" },
+    { "name": "Pass from Opp<br>Alliance Zone", "code": "poa", "type": "bool" }
   ],
   "endgame": [
-    { "name": "Climb",
-      "code": "tc",
-      "type": "radio",
-      "choices": {
-        "1": "Level 1<br>",
-        "2": "Level 2<br>",
-        "3": "Level 3<br>",
-        "a": "Attempted<br>",
-        "x": "Not Attempted"
-      },
-      "defaultValue": "x"
-    }
+    { "name": "Climb", "code": "tc", "type": "radio", "choices": { "1": "Level 1<br>", "2": "Level 2<br>", "3": "Level 3<br>", "a": "Attempted<br>", "x": "Not Attempted" }, "defaultValue": "x" }
   ],
   "postmatch": [
-  { "name": "Citrus Ranking",
-      "code": "cr",
-      "type": "radio",
-      "choices": {
-        "1": "1 (best)<br>",
-        "2": "2<br>",
-        "3": "3<br>"
-      },
-      "defaultValue":"3"
-    },
-    { "name": "Driver Skill",
-      "code": "ds",
-      "type": "radio",
-      "choices": {
-        "n": "Not Effective<br>",
-        "a": "Average<br>",
-        "v": "Very Effective<br>",
-        "x": "Not Observed"
-      },
-      "defaultValue": "x"
-    },
-    { "name": "Defense Rating",
-      "code": "dr",
-      "type": "radio",
-      "choices": {
-        "b": "Below Average<br>",
-        "a": "Average<br>",
-        "g": "Good<br>",
-        "e": "Excellent<br>",
-        "x": "Did not play defense"
-      },
-      "defaultValue": "x"
-    },
-    { "name": "Speed Rating",
-      "code": "sr",
-      "type": "radio",
-      "choices": {
-        "1": "1 (slow)<br>",
-        "2": "2<br>",
-        "3": "3<br>",
-        "4": "4<br>",
-        "5": "5 (fast)"
-      },
-      "defaultValue":"3"
-    },
-    { "name": "Crossed Bump",
-      "code": "bmp",
-      "type": "bool"
-    },
-    { "name": "Crossed Trench",
-      "code": "tre",
-      "type": "bool"
-    },
-    { "name": "Died/Immobilized",
-      "code": "die",
-      "type": "bool"
-    },
-    { "name": "Tippy<br>(almost tipped over)",
-      "code": "tip",
-      "type": "bool"
-    },
-    { "name": "Was Defended",
-      "code": "def",
-      "type": "bool"
-    },
-    { "name": "Excessive Penalties",
-      "code": "pen",
-      "type": "bool"
-    },
-    { "name": "What percentage of the total fuel for this alliance did this robot score?",
-      "tooltip": "What percentage of the total fuel for this alliance did this robot score?",
-      "code": "pct",
-      "type": "number",
-      "min": 0,
-      "max": 100
-    },
-    { "name": "Match Review Needed? Too hard to scout? Fell asleep? Went to bathroom? Not paying attention?",
-      "code": "mrn",
-      "type": "bool"
-    },
-    { "name": "Comments",
-      "code": "co",
-      "type": "text",
-      "size": 15,
-      "maxSize": 55
-    }
+    { "name": "Citrus Ranking", "code": "cr", "type": "radio", "choices": { "1": "1 (best)<br>", "2": "2<br>", "3": "3<br>" }, "defaultValue": "3" },
+    { "name": "Driver Skill", "code": "ds", "type": "radio", "choices": { "n": "Not Effective<br>", "a": "Average<br>", "v": "Very Effective<br>", "x": "Not Observed" }, "defaultValue": "x" },
+    { "name": "Defense Rating", "code": "dr", "type": "radio", "choices": { "b": "Below Average<br>", "a": "Average<br>", "g": "Good<br>", "e": "Excellent<br>", "x": "Did not play defense" }, "defaultValue": "x" },
+    { "name": "Speed Rating", "code": "sr", "type": "radio", "choices": { "1": "1 (slow)<br>", "2": "2<br>", "3": "3<br>", "4": "4<br>", "5": "5 (fast)" }, "defaultValue": "3" },
+    { "name": "Crossed Bump", "code": "bmp", "type": "bool" },
+    { "name": "Crossed Trench", "code": "tre", "type": "bool" },
+    { "name": "Died/Immobilized", "code": "die", "type": "bool" },
+    { "name": "Tippy<br>(almost tipped over)", "code": "tip", "type": "bool" },
+    { "name": "Was Defended", "code": "def", "type": "bool" },
+    { "name": "Excessive Penalties", "code": "pen", "type": "bool" },
+    { "name": "What percentage of the total fuel for this alliance did this robot score?", "tooltip": "What percentage of the total fuel for this alliance did this robot score?", "code": "pct", "type": "number", "min": 0, "max": 100 },
+    { "name": "Match Review Needed? Too hard to scout? Fell asleep? Went to bathroom? Not paying attention?", "code": "mrn", "type": "bool" },
+    { "name": "Comments", "code": "co", "type": "text", "size": 15, "maxSize": 55 }
   ]
 }`;
