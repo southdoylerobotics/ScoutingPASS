1cbc7685883507fead4daa00586658d01f949423
South-Doyle Robotics <34195060+southdoylerobotics@users.noreply.github.com>
2026-04-09 23:53:33 -0400

    Change team number type from radio to number

--- 2026/rebuilt_config.js
+++ 2026/rebuilt_config.js
@@ -5,7 +5,7 @@ var config_data = `{
   "checkboxAs": "10",
   "prematch": [
     { "name": "First Name", "code": "s", "type": "scouter", "size": 5, "maxSize": 5, "required": "true" },
-    { "name": "Your Team #", "code": "yt", "type": "radio", "choices": { "2393": "2393", "6517": "6517" }, "defaultValue": "ca", "required": "true" },
+    { "name": "Your Team #", "code": "yt", "type": "number", "choices": {"ca": "2393", "so":"6517"},
     { "name": "Event", "code": "e", "type": "event", "defaultValue": "2026alhu", "required": "true" },
     { "name": "Match Level", "code": "l", "type": "level", "choices": { "qm": "Quals<br>", "sf": "Semifinals<br>", "f": "Finals" }, "defaultValue": "qm", "required": "true" },
     { "name": "Match #", "code": "m", "type": "match", "min": 1, "max": 150, "required": "true" },
@@ -18,7 +18,7 @@ var config_data = `{
     { "name": "Won Auto?", "code": "wau", "type": "bool" }
   ],
   "teleop": [
-    { "name": "Fuel Scored", "code": "tsf", "type": "counter", "hopperButtons": true }
+    { "name": "Fuel Scored", "code": "tsf", "type": "counter", "hopperButtons": true },
   ],
   "endgame": [
     { "name": "Climb", "code": "tc", "type": "radio", "choices": { "1": "Level 1<br>", "2": "Level 2<br>", "3": "Level 3<br>", "a": "Attempted<br>", "x": "Not Attempted" }, "defaultValue": "x" }
